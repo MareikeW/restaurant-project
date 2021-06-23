30b8a40c3966eaab7e994c96e49c7cb032dc979b
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-06-23 09:20:48 +0200

    init react + react-router + styled-components installation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import GlobalStyle from "./globalStyles";
+
+
+/* Add react router here */
+/* Add global styles component here */
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
